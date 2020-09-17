c5815c51a1af3e3b36d88a1ce0b2cb6e71cd0934
Jake Nguyen <69091111+jakenguyen0118@users.noreply.github.com>
2020-09-17 17:17:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,23 @@ let sheetAsJSON =
 $.ajax({url: sheetAsJSON}).then(data => {
 	console.log(data.feed.entry);
 
+	// div that holds the projects
 	const $project = $('.projectsContainer');
+	// forEach method to loop through array
 	const projects = data.feed.entry.forEach(project => {
+		// div to hold the project description/anchor/img
 		const $newProject = $('<div>').addClass('addedProjects');
+		// anchor tag to hold img to link to codepen						   open in new tab
 		const $projectLink = $('<a>').attr('href', project.gsx$link.$t).attr('target', '_blank')
+		// used imgur to upload photo of project and uploaded to google sheet
 		const $projectImage = $('<img>').attr('src', project.gsx$image.$t).addClass('img');
+		// add project description to project div
 		$newProject.text(project.gsx$description.$t).addClass('img-description');
+		// adding individual project div to project container
 		$project.append($newProject);
+		// append anchor tag to individual project divs
 		$newProject.append($projectLink);
+		// append img to anchors
 		$projectLink.append($projectImage);
 	})
 })
